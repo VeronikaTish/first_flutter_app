d7476036c7bcceeef4b8b43278e6a073984aa081
Veronika Tishkovskaya <tishkovskaya.v@gmail.com>
2023-12-19 22:20:33 +0100

    style text and background

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,28 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.bottomLeft,
+              end: Alignment.topRight,
+              colors: [
+                Color.fromARGB(255, 212, 79, 38),
+                Color.fromARGB(255, 231, 139, 97)
+              ],
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              'Hello World!',
+              style: TextStyle(
+                fontSize: 28,
+                color: Colors.white,
+              ),
+            ),
+          ),
         ),
       ),
     ),
